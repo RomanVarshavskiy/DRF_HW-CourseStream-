a14ab3ebbf7bcf8d3025474008eccd4c03884000
Roman Varshavski <raman.varshavskiy@gmail.com>
2025-11-25 20:54:32 +0100

    update serializers.py, create Payments model

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,31 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons = SerializerMethodField()
+    count_lesson = SerializerMethodField()
+
+    def get_lessons(self, course):
+        return [lesson.name for lesson in Lesson.objects.filter(course=course)]
+
+    def get_count_lesson(self, course):
+        return Lesson.objects.filter(course=course).count()
+
     class Meta:
         model = Course
         fields = "__all__"
 
 
 class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
+class LessonDetailSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
