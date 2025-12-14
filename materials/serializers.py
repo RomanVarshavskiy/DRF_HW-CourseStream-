2ebf018f0ef64cf335452722080b65c05047c668
Roman Varshavski <raman.varshavskiy@gmail.com>
2025-12-14 21:33:43 +0100

    create Subscription model, SubscriptionAPIView, update CourseSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,14 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import ModelSerializer
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.validators import validate_link_verification
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     lessons = SerializerMethodField()
     count_lesson = SerializerMethodField()
+    subscription = SerializerMethodField()
 
     def get_lessons(self, course):
         return [lesson.name for lesson in Lesson.objects.filter(course=course)]
@@ -14,18 +16,28 @@ class CourseSerializer(ModelSerializer):
     def get_count_lesson(self, course):
         return Lesson.objects.filter(course=course).count()
 
+    def get_subscription(self, course):
+        request = self.context.get("request")
+
+        if request and request.user.is_authenticated:
+            return Subscription.objects.filter(user=request.user, course=course).exists()
+        return False
+
+
     class Meta:
         model = Course
         fields = "__all__"
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
+    video = serializers.URLField(validators=[validate_link_verification])
+
     class Meta:
         model = Lesson
         fields = "__all__"
 
 
-class LessonDetailSerializer(ModelSerializer):
+class LessonDetailSerializer(serializers.ModelSerializer):
     course_info = SerializerMethodField()
 
     def get_course_info(self, obj):
@@ -40,3 +52,4 @@ class LessonDetailSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = ("id", "name", "description", "preview", "video", "course_info")
+
--- materials/views.py
+++ materials/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework import status
+from rest_framework.generics import (CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView,
+                                     get_object_or_404)
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.serializers import CourseSerializer, LessonDetailSerializer, LessonSerializer
 from users.permissions import IsModer, IsOwner
 
@@ -49,7 +51,7 @@ class LessonListAPIView(ListAPIView):
     serializer_class = LessonSerializer
 
     def get_queryset(self):
-        if  self.request.user.is_superuser or self.request.user.groups.filter(name="moders").exists():
+        if self.request.user.is_superuser or self.request.user.groups.filter(name="moders").exists():
             return Lesson.objects.all()
         return Lesson.objects.filter(owner=self.request.user)
 
@@ -69,3 +71,27 @@ class LessonUpdateAPIView(UpdateAPIView):
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     permission_classes = (~IsModer | IsOwner,)
+
+
+class SubscriptionAPIView(CreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        course_id = request.data.get("course_id")
+        if not course_id:
+            return Response({"error": "course_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        course_item = get_object_or_404(Course, id=course_id)
+
+        subs_item = Subscription.objects.filter(user=user, course=course_item)
+
+        # Если подписка у пользователя на этот курс есть - удаляем ее
+        if subs_item.exists():
+            subs_item.delete()
+            message = "подписка удалена"
+        # Если подписки у пользователя на этот курс нет - создаем ее
+        else:
+            Subscription.objects.create(user=user, course=course_item)
+            message = "подписка добавлена"
+        # Возвращаем ответ в API
+        return Response({"message": message}, status=status.HTTP_200_OK)
