265a2b2784252671b0f2c284332b4041593abea5
Roman Varshavski <raman.varshavskiy@gmail.com>
2026-01-11 00:02:51 +0100

    Добавил ALLOWED_HOSTS и обновил настройки

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "web", "nginx", "0.0.0.0"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 INSTALLED_APPS = [
